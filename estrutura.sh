0f0034de1d0b87d9a9868ace1c83108b5c65ee45
David Pereira <115894272+Devideos@users.noreply.github.com>
2025-04-20 10:46:36 -0300

    alterando as senhas estrutura.sh

--- estrutura.sh
+++ estrutura.sh
@@ -15,19 +15,33 @@ groupadd GRP_SEC
 
 echo "Criando usuários..."
 
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd carlos -m -s /bin/bash -G GRP_ADM
+useradd maria -m -s /bin/bash -G GRP_ADM
+useradd joao -m -s /bin/bash -G GRP_ADM
 
-useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd debora -m -s /bin/bash -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -G GRP_VEN
+useradd roberto -m -s /bin/bash -G GRP_VEN
 
-useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd josefina -m -s /bin/bash -G GRP_SEC
+useradd amanda -m -s /bin/bash -G GRP_SEC
+useradd rogerio -m -s /bin/bash -G GRP_SEC
 
-echo "Especificando permissões dos diretórios...."
+echo "Definindo senhas para os usuários..."
+
+echo "carlos:Senha123" | chpasswd
+echo "maria:Senha123" | chpasswd
+echo "joao:Senha123" | chpasswd
+
+echo "debora:Senha123" | chpasswd
+echo "sebastiana:Senha123" | chpasswd
+echo "roberto:Senha123" | chpasswd
+
+echo "josefina:Senha123" | chpasswd
+echo "amanda:Senha123" | chpasswd
+echo "rogerio:Senha123" | chpasswd
+
+echo "Especificando permissões dos diretórios..."
 
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
@@ -39,3 +53,4 @@ chmod 770 /sec
 chmod 777 /publico
 
 echo "Fim....."
+
